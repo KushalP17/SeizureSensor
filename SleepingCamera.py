4cba68942997b2860a2f28b0e3cfaaa9a76c7933
Kushal Parekh <kup9@pitt.edu>
2024-09-28 17:42:05 -0400

    V2 Demo Conditioning

--- SleepingCamera.py
+++ SleepingCamera.py
@@ -3,11 +3,12 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-# image_filter = PREVIEW
 alive = True
+
 # fgbg = cv.createBackgroundSubtractorKNN(detectShadows=True)
 fgbg = cv.createBackgroundSubtractorMOG2()
 kernel = np.ones((3,3),np.uint8)
+
 running_frame_average = 0
 steady_motion_frames = 0
 total_frames = 1
@@ -19,17 +20,25 @@ running_frame_average_list = [0]
 
 win_name = "Sleeping Seizure"
 cv.namedWindow(win_name, cv.WINDOW_NORMAL)
-# cv.namedWindow("running avg", cv.WINDOW_NORMAL)
 # source = cv.VideoCapture('testvideos/seizure_man_2.mp4')
-source = cv.VideoCapture('testvideos/seizure_woman.mp4')
-# source = cv.VideoCapture(1)
+
+
+run_live_video = False
+show_background_cutout = False
+
+if run_live_video:
+    source = cv.VideoCapture(1)
+else:
+    vid_src = "File"
+    source = cv.VideoCapture('testvideos/seizure_woman.mp4')
+
 
 
 while alive:
     has_frame, frame = source.read()
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    if not has_frame:
-        break 
+    if not has_frame and vid_src == "File":
+        source = cv.VideoCapture('testvideos/seizure_woman.mp4')
         
     # frame = cv.flip(frame, 1)
 
@@ -40,11 +49,14 @@ while alive:
     fgmask=(cv.morphologyEx(fgmask,cv.MORPH_CLOSE,kernel,iterations=10)>100)
 
     # use the mask to remove the background
-    frame[:,:]=frame[:,:]*fgmask
+    analyze_frame=frame[:,:]*fgmask
     # frame[:,:,1]=frame[:,:,1]*fgmask
     # frame[:,:,2]=frame[:,:,2]*fgmask
 
-    frame_average = np.average(frame)
+    if show_background_cutout:
+        frame = analyze_frame    
+
+    frame_average = np.average(analyze_frame)
     # print(frame_average)
 
     if frame_average > 1 or frame_average > running_frame_average:
@@ -53,7 +65,9 @@ while alive:
         steady_motion_frames = 0
 
     if steady_motion_frames > 40:
-        print("Seizure Motion Detected")
+        pass # Function for Seizure Motion Detection
+    else:
+        pass # Function for No Motion Detected
 
     if len(frame_avg_list) > 1:
         frame_avg_list.remove(np.max(frame_avg_list))
@@ -70,13 +84,22 @@ while alive:
     cv.imshow(win_name, frame)
 
     # print(f"RFA: {running_frame_average}")
-    # print(f"FA: {frame_average}")qqqqq
+    # print(f"FA: {frame_average}")
 
     
 
     # print(running_frame_average)
 
-    key = cv.waitKey(25)
+    key = cv.waitKey(20)
     if key == ord("Q") or key == ord("q") or key == 27:
         alive = False
+
+    if key == ord("0") or key == 48 or key == 96:
+        source = cv.VideoCapture(1)
+    elif key == ord("1") or key == 49 or key == 97:
+        source = cv.VideoCapture('testvideos/seizure_woman.mp4')
+    
+    if key == ord("2") or key == 50 or key == 98:
+        show_background_cutout = not show_background_cutout
+
 
