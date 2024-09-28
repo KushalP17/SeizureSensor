ba1a0ea04b422b96d7c13b38d652d3b786abb0fa
Tyler Hansen <122710485+thans28@users.noreply.github.com>
2024-09-28 13:22:03 -0400

    ESP32 Code w/ EMG sensing
    Pre-MongoDB implementation

---
+++ SeizeSensor_EMG_Sketch.ino
@@ -0,0 +1,18 @@
+
+
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial); // optionally wait for serial terminal to open
+  Serial.println("MyoWare Example_01_analogRead_SINGLE");
+
+}
+
+void loop() {
+  int sensorValue = analogRead(13); // read the input on analog pin A0
+
+  Serial.println(sensorValue); // print out the value you read
+
+  delay(50); // to avoid overloading the serial terminal
+
+}
